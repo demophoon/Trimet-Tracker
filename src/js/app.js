ad00a4284038107e43bda214d3779841aedd5576
Britt Gresham <brittcgresham@gmail.com>
2015-10-25 16:34:32 -0700

    Updated app.

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,8 @@ var colors = {
     trimetBlue: "#084C8D",
     trimetOrange: "#EE5917",
     trimetWhite: "#FFFFFF",
+    trimetBlack: "#000000",
+    trimetGrey: "#404040",
 };
 
 var transit_colors = {
@@ -36,14 +38,15 @@ var transit_colors = {
  */
 
 
+
 function serialize(o) {
-   var str = [];
+   var str_parts = [];
    for(var p in o){
        if (o.hasOwnProperty(p)) {
-           str.push(encodeURIComponent(p) + "=" + encodeURIComponent(o[p]));
+           str_parts.push(encodeURIComponent(p) + "=" + encodeURIComponent(o[p]));
        }
    }
-   return str.join("&");
+   return str_parts.join("&");
 }
 
 var Trimet = function(apikey) {
@@ -59,10 +62,12 @@ var Trimet = function(apikey) {
       urlparams['json'] = true;
 
       var null_callback = function(data, status, request) {};
-      success_callback = urlparams['success'] || null_callback;
-      error_callback = urlparams['error'] || null_callback;
+      success_callback = options['success'] || null_callback;
+      error_callback = options['error'] || null_callback;
 
       var url = self.baseurl + endpoint + '?' + serialize(urlparams);
+
+      console.log(url);
 
       ajax({
           url: url,
@@ -78,8 +83,8 @@ var Trimet = function(apikey) {
               ll: lon + "," + lat,
               feet: radiusft,
               showRouteDirs: true,
-              success: callback,
-          }
+          },
+          success: callback,
       });
   }
 
@@ -87,8 +92,8 @@ var Trimet = function(apikey) {
       return self._request('arrivals', {
           urlparams: {
               locids: location_ids.join(','),
-              success: callback,
-          }
+          },
+          success: callback,
       });
   }
 };
@@ -96,42 +101,68 @@ var Trimet = function(apikey) {
 var trimet = new Trimet(appid);
 var selected_route = null;
 
+function get_data(key, def) {
+    var data = localStorage.getItem('pdxtransit');
+    if (data == null) {
+        data = {};
+    } else {
+        data = JSON.parse(data);
+    }
+    return data[key] || def;
+}
+
+function save_data(key, value) {
+    var data = localStorage.getItem('pdxtransit');
+    if (data == null) {
+        data = {};
+    } else {
+        data = JSON.parse(data);
+    }
+    data[key] = value
+    localStorage.setItem('pdxtransit', JSON.stringify(data));
+}
+
 /*
  * End of Trimet helpers
  **************************************************/
 
-
 var loading = new UI.Card({
-  title: 'Trimet Tracker',
-  subtitle: 'Britt Gresham',
-  body: 'One moment\nFinding stops near you...',
-  backgroundColor: colors.trimetBlue,
-  titleColor: colors.trimetWhite,
-  subtitleColor: colors.trimetWhite, // Named colors
-  bodyColor: colors.trimetWhite, // Hex colors
-  style: 'small',
+    title: 'Trimet Tracker',
+    subtitle: 'Britt Gresham',
+    body: 'One moment\nFinding stops near you...',
+    backgroundColor: colors.trimetBlue,
+    titleColor: colors.trimetWhite,
+    subtitleColor: colors.trimetWhite, // Named colors
+    bodyColor: colors.trimetWhite, // Hex colors
+    style: 'small',
 });
 
-loading.show();
+function init() {
+    loading.show();
 
-navigator.geolocation.getCurrentPosition(
-    function (pos) {
-        trimet.stops(pos.coords.latitude, pos.coords.longitude, showRoutes);
-    },
-    function (err) {
-        console.warn(err.message);
-    },
-    {
-        enableHighAccuracy: true,
-        timeout: 15000,
-        maximumAge: 0,
-    }
-);
+    //save_data('favorite_lines', []);
 
+    navigator.geolocation.getCurrentPosition(
+        function (pos) {
+            trimet.stops(pos.coords.latitude, pos.coords.longitude, showRoutes);
+        },
+        function (err) {
+            console.warn(err.message);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 15000,
+            maximumAge: 0,
+        }
+    );
+}
 
 function showRoutes(data) {
     var resultSet = data['resultSet']['location'];
-    var routes = {};
+    var routes = {
+        order: [],
+    };
+
     for (var i in resultSet) {
         var stop = resultSet[i];
 
@@ -141,6 +172,12 @@ function showRoutes(data) {
 
         for (var n in stop['route']) {
             var route = stop['route'][n];
+            route['route'] = route['route'].toString();
+            route['line'] = route['route'].toString();
+
+            if (route['desc'].toLowerCase() == 'max shuttle') {
+                continue;
+            }
 
             // Separate by route
             if (route['type'] != 'B') {
@@ -149,12 +186,16 @@ function showRoutes(data) {
             }
 
             if (routes[route['route']] == null) {
-                if (Object.keys(routes).length >= 15) {
-                    continue;
-                }
+                //if (routes.order.length >= 15) {
+                //    continue;
+                //}
+                routes.order.push(route['route']);
                 routes[route['route']] = {
                     type: route['type'],
-                    to: {},
+                    line: route['line'],
+                    to: {
+                        order: [],
+                    },
                     desc: route['desc'],
                 };
             }
@@ -163,6 +204,7 @@ function showRoutes(data) {
             for (var d in route['dir']) {
                 dir = route['dir'][d];
                 if (routes[route['route']]['to'][dir['dir']] == null) {
+                    routes[route['route']]['to'].order.push(dir['dir']);
                     routes[route['route']]['to'][dir['dir']] = {
                         name: dir['desc'],
                         stops: [],
@@ -177,59 +219,98 @@ function showRoutes(data) {
         }
     }
 
+    rendered = [];
+    function _render_line_item(name) {
+        var ret = {
+            title: name,
+            route: routes[name],
+        }
+        if (routes[name]['desc']) {
+            if (routes[name]['desc'].split('-')[0] == name) {
+                var subtitle = routes[name]['desc'].split('-')
+                subtitle.shift(0);
+                ret['subtitle'] = subtitle.join(' ');
+            } else {
+                ret['subtitle'] = routes[name]['desc'];
+            }
+        }
+
+        if (routes[name]['type'] == 'R') {
+            if (ret['title'].split(' ')[0].toLowerCase() == 'max') {
+                var parts = ret['title'].split(' ');
+                parts.shift(0);
+                ret['title'] = parts.join(' ');
+                ret['icon'] = 'images/max_' + parts[0].toLowerCase() + '.png';
+            } else if (ret['title'].split('-')[0].trim().toLowerCase() == 'portland streetcar') {
+                var parts = ret['title'].split('-');
+                ret['title'] = parts[1].trim();
+                ret['icon'] = 'images/trimet_rail.png';
+            }
+        }
+        rendered.push(routes[name]);
+        return ret;
+    }
+
+    sections = [];
+    favorites = get_data('favorite_lines', []);;
+
+    if (favorites.length > 0) {
+        sections.push({
+            title: 'Favorites',
+            items: routes.order.filter(function(item) {
+                return rendered.indexOf(item) == -1 && favorites.indexOf(item) != -1;
+            }).map(_render_line_item),
+        });
+    }
+
+    sections.push({
+        title: 'Lines',
+        items: routes.order.filter(function(item) {
+            return rendered.indexOf(item) == -1;
+        }).map(_render_line_item),
+    });
+
     var routeMenu = new UI.Menu({
-        sections: [{
-            title: 'Lines',
-            items: Object.keys(routes).map(function(name) {
-                var ret = {
-                    title: name,
-                    route: routes[name],
-                    routes
-                }
-                if (routes[name]['desc']) {
-
-                    if (routes[name]['desc'].split('-')[0] == name) {
-                        var desc = routes[name]['desc'].split('-')
-                        desc.shift(0);
-                        ret['subtitle'] = desc.join(' ');
-                    } else {
-                        ret['subtitle'] = routes[name]['desc'];
-                    }
-                }
-
-                ret['line'] = ret['title'];
-
-                if (routes[name]['type'] == 'R') {
-                    if (ret['title'].split(' ')[0].toLowerCase() == 'max') {
-                        var parts = ret['title'].split(' ');
-                        parts.shift(0);
-                        ret['title'] = parts.join(' ');
-                        ret['icon'] = 'images/max_' + parts[0].toLowerCase() + '.png';
-                    } else if (ret['title'].split('-')[0].trim().toLowerCase() == 'portland streetcar') {
-                        var parts = ret['title'].split('-');
-                        ret['title'] = parts[1].trim();
-                        ret['icon'] = 'images/trimet_rail.png';
-                    }
-                }
-                return ret;
-            }),
-        }],
+        backgroundColor: colors.trimetWhite,
+        textColor: colors.trimetBlack,
+        highlightBackgroundColor: colors.trimetBlue,
+        highlightTextColor: colors.trimetWhite,
+        sections: sections,
     });
 
     routeMenu.on('select', function(e) {
-        selected_route = e.item.line;
+        selected_route = e.item.route.line;
         showDirections(e.item.route);
     });
 
-    loading.hide();
+    routeMenu.on('longSelect', function(e) {
+        return;
+        var favorites = get_data('favorite_lines', []);
+        if (favorites.indexOf(e.item.title) == -1) {
+            favorites.push(e.item.title);
+        } else {
+            favorites = favorites.filter(function(item) {
+                return item != e.item.title;
+            });
+        }
+        save_data('favorite_lines', favorites);
+        routeMenu.hide();
+        init();
+    });
+
     routeMenu.show();
+    loading.hide();
 }
 
 function showDirections(route) {
     var directionsMenu = new UI.Menu({
+        backgroundColor: colors.trimetWhite,
+        textColor: colors.trimetBlack,
+        highlightBackgroundColor: colors.trimetBlue,
+        highlightTextColor: colors.trimetWhite,
         sections: [{
             title: route['desc'],
-            items: Object.keys(route['to']).map(function(dir) {
+            items: route['to'].order.map(function(dir) {
                 ret = {
                     title: route['to'][dir]['name'],
                     direction: route['to'][dir],
@@ -289,6 +370,10 @@ function showStops(stops) {
     }
 
     var stopsMenu = new UI.Menu({
+        backgroundColor: colors.trimetWhite,
+        textColor: colors.trimetBlack,
+        highlightBackgroundColor: colors.trimetBlue,
+        highlightTextColor: colors.trimetWhite,
         sections: Object.keys(sections).map(function(key) {
             return sections[key];
         }),
@@ -306,17 +391,31 @@ function showStopInformation(data) {
     scheduled = [];
     for (var i in resultSet) {
         arrival = resultSet[i];
-        if (arrival['route'] != selected_route && selected_route.split(" ")[0] != 'MAX') {
+        if (arrival['route'] != selected_route) {
             continue;
         }
         scheduled.push({
+            departed: arrival['departed'],
             fullSign: arrival['fullSign'],
             shortSign: arrival['shortSign'],
             status: arrival['status'],
+            route: arrival['route'],
             locid: arrival['locid'],
             estimated: new Date(arrival['estimated']),
             scheduled: new Date(arrival['scheduled']),
         });
+    }
+
+    function _body_info(detail) {
+        var lines = []
+        if (scheduled.length > 1) {
+            for (var x=1; x<scheduled.length; x+=1) {
+                lines.push(est_or_arr(scheduled[x]));
+            }
+        }
+        lines.push("");
+        lines.push("Stop ID: " + detail['locid']);
+        return lines.join("\n");
     }
 
     if (scheduled.length > 0) {
@@ -324,13 +423,22 @@ function showStopInformation(data) {
         var detailCard = new UI.Card({
             title: detail['shortSign'],
             subtitle: est_or_arr(detail),
-            body: "Status: " + detail['status'] + "\n" + "Stop ID: " + detail['locid'],
+            body: _body_info(detail),
             style: 'small',
             scrollable: true,
         });
         setInterval(function() {
             detailCard.subtitle(est_or_arr(detail));
+            detailCard.body(_body_info(detail));
         }, 1000);
+        if (detail['fullSign'].split(" ")[0].toLowerCase() == "max") {
+            parts = detail['fullSign'].split(" ");
+            parts.shift(0);
+            while (parts[0] == "") {
+                parts.shift(0);
+            }
+            detailCard.icon('images/max_' + parts[0].toLowerCase() + '.png');
+        }
     } else {
         var detailCard = new UI.Card({
             title: 'No scheduled arrivals.',
@@ -339,14 +447,6 @@ function showStopInformation(data) {
             scrollable: true,
         });
     }
-    if (detail['fullSign'].split(" ")[0].toLowerCase() == "max") {
-        parts = detail['fullSign'].split(" ");
-        parts.shift(0);
-        while (parts[0] == "") {
-            parts.shift(0);
-        }
-        detailCard.icon('images/max_' + parts[0].toLowerCase() + '.png');
-    }
     detailCard.show();
 }
 
@@ -354,7 +454,7 @@ function est_or_arr(detail) {
     var now = new Date();
     var est = (detail['estimated'].getTime() - now.getTime()) / 1000;
     var sch = (detail['scheduled'].getTime() - now.getTime()) / 1000;
-    if (detail['departed'] || est < sch) {
+    if (detail['departed']) {
         return 'Arr: ' + seconds_to_human(est).trim();
     } else {
         return 'Sch: ' + seconds_to_human(sch).trim();
@@ -372,3 +472,7 @@ function seconds_to_human(seconds) {
         return "Due";
     }
 }
+
+
+// Boilerplate for initialization
+init();
